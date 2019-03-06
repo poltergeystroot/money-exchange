035a2b80d1b56315375629795afd09f79bb16567
poltergeystroot <47752576+poltergeystroot@users.noreply.github.com>
2019-03-07 01:57:42 +0300

    add: result exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var denominations = [1, 5, 10, 25, 50];
+    var namecoins = ['P', 'N', 'D', 'Q', 'H'];
+    var resultcoin = {};
+    if (currency > 10000) {
+        resultcoin={"error": "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    else if(currency <= 0){resultcoin={};}
+    else{
+        while (currency > 0) {
+            var coin = denominations.pop();
+            var coin1 = namecoins.pop();
+            var count = Math.floor(currency/coin);
+            currency -= count * coin;
+            if (count) {resultcoin[coin1] = count;}
+        }
+    }
+    return resultcoin;
 }
